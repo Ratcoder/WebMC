fff05c86a267bd147ef1d2b407aaaaf9224cc478
Ratcoder <69884239+Ratcoder@users.noreply.github.com>
2020-12-24 17:33:13 -0600

    removed uneccary imports and comments

--- app/server.js
+++ app/server.js
@@ -1,11 +1,2 @@
-const pidusage = require('pidusage');
-const schedule = require('node-schedule');
-const fs = require('fs');
-
-
-// ---------- MINECRAFT ----------
 let mc = require('./minecraft')();
-// ---------- WEBSITE ----------
-require('./website')({
-    mc
-});
+require('./website')({mc});
