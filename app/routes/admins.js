7fe25ba3318c1ad3debf61fef0a26220de890520
Ratcoder <69884239+Ratcoder@users.noreply.github.com>
2021-04-01 00:16:41 -0500

    a route sends a list of admins

---
+++ app/routes/admins.js
@@ -0,0 +1,15 @@
+const Database = require('../services/database');
+
+module.exports = {
+    path: '/api/admins',
+    method: 'GET',
+    accessLevel: 1,
+    handler: async (request, responce) => {
+        const adminsUnfiltered = await Database.admins.getAll();
+        const admins = [];
+        adminsUnfiltered.forEach(element => {
+            admins.push({name: element.name, level: element.level})
+        });
+        responce.status(200).json(admins);
+    }
+}
