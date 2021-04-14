181c01b09f9392c6eb10e9259a0a0a92fa090865
Ratcoder <69884239+Ratcoder@users.noreply.github.com>
2021-04-13 20:00:09 -0500

    install.js no longer starts minecraft server

--- app/scripts/install.js
+++ app/scripts/install.js
@@ -1,4 +1,4 @@
-const Database = require('../services/database');
+const Admins = require('../services/database/admins');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
@@ -6,5 +6,5 @@ let admin = {name: process.argv[2], password: process.argv[3], level: 3};
 
 bcrypt.hash(admin.password, saltRounds, async (err, hash) => {
     admin.password = hash;
-    await Database.admins.insert(admin);
+    await Admins.insert(admin);
 });
