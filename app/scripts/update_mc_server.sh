ed535664ae63afb00fd3a052c4fabf14e48e1cf1
Ratcoder <69884239+Ratcoder@users.noreply.github.com>
2021-05-05 16:08:14 -0500

    mc/bedrock-server/ is cleared before updating

--- app/scripts/update_mc_server.sh
+++ app/scripts/update_mc_server.sh
@@ -1,3 +1,5 @@
 curl --output ./mc/bedrock-server.zip $(./app/scripts/get_mc_dl_url.sh)
+rm -r mc/bedrock-server/
+mkdir mc/bedrock-server/
 unzip -o ./mc/bedrock-server.zip -d ./mc/bedrock-server/
 chmod +x ./mc/bedrock-server/bedrock_server
