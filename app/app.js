2a197dff7af4e9efbe0f34ec5cb6b9cb1b6dd801
Ratcoder <69884239+Ratcoder@users.noreply.github.com>
2021-05-17 15:47:29 -0500

    console.log disabled in production

--- app/app.js
+++ app/app.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
 
+console.log('Welcome to WebMC!\nWebMC is running at this url: https://localhost:14142');
+if(process.argv[2] != 'dev') console.log = () => {};
+
 require('./server');
 fs.readdirSync('./app/services').forEach(file => require('./services/' + file));
