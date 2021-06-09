4d4e0ad1df5087a756b9dbb14d8d271dc5c86c10
Ratcoder <69884239+Ratcoder@users.noreply.github.com>
2021-06-09 15:19:12 -0500

    spruced up production terminal output

--- app/app.js
+++ app/app.js
@@ -1,7 +1,40 @@
 const fs = require('fs');
 
-console.log('Welcome to WebMC!\nWebMC is running at this url: https://localhost:14142');
-if(process.argv[2] != 'dev') console.log = () => {};
+const lc = "\x1b[32m";
+const rc = "\x1b[0m";
 
+const log = console.log;
+function updateDisplay(status) {
+    let statusTexts = {
+        'online':        '│              online              │',
+        'offline':       '│              offline             │',
+        'restarting':    '│            restarting            │',
+        'shutting-down': '│           shutting-down          │',
+        'starting':      '│             starting             │'
+    };
+
+    log(`
+        ${lc}┌──────────────────────────────────┐
+        ${lc}│                                  │
+        ${lc}│        ${rc}\x1b[1mWelcome to WebMC!${rc}${lc}         │
+        ${lc}│                                  │
+        ${lc}│   ${rc}WebMC is running at this url:${lc}  │
+        ${lc}│      ${rc}https://localhost:14142${lc}     │
+        ${lc}│                                  │
+        ${lc}${statusTexts[status]}
+        ${lc}│                                  │
+        ${lc}└──────────────────────────────────┘`
+    );
+}
+
+if (process.argv[2] != 'dev') {
+    updateDisplay('offline');
+    const Events = require('./services/events')
+    Events.on('minecraft_status', status => {
+        console.clear();
+        updateDisplay(status);
+    });
+    console.log = () => {};
+}
 require('./server');
 fs.readdirSync('./app/services').forEach(file => require('./services/' + file));
