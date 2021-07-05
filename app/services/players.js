299f0e142774bf8eb2b36c0bb574b62c9139a41d
Ratcoder <69884239+Ratcoder@users.noreply.github.com>
2021-07-05 16:30:32 -0500

    player permissions now work properly for players added from the ui

--- app/services/players.js
+++ app/services/players.js
@@ -3,22 +3,38 @@ const Minecraft = require("./minecraft");
 const Database = require('./database');
 const fs = require('fs');
 const util = require('util');
-const players = require("../routes/players");
 const writeFile = util.promisify(fs.writeFile);
 
 const connectedBannedPlayersIntervals = new Map();
 
 Events.on('minecraft_logs_playerConnected', async (name, xuid) => {
-    let player = await Database.players.get(xuid);
+    let player = await Database.players.get(name);
 
-    if(!player){
+    if (!player) {
         add(name, await Database.settings.get('defaultPlayerPermissionLevel'));
     }
-    else if(player.ban){
-        connectedBannedPlayersIntervals.set(name, 
-            setInterval(() => {
-                Minecraft.process.stdin.write(`kick ${name} You have been banned: ${player.ban.reason}\n`);
-        }, 1000));
+    else {
+        if (!player.xuid) {
+            player.xuid = xuid;
+            await Database.players.update(name, player);
+            await writePermissionsFile();
+        }
+        if (player.xuid != xuid) {
+            connectedBannedPlayersIntervals.set(
+                name, 
+                setInterval(() => {
+                    Minecraft.process.stdin.write(`kick ${name} Your xuid does not match.\n`);
+                }, 1000)
+            );
+        }
+        else if (player.ban) {
+            connectedBannedPlayersIntervals.set(
+                name, 
+                setInterval(() => {
+                    Minecraft.process.stdin.write(`kick ${name} You have been banned: ${player.ban.reason}\n`);
+                }, 1000)
+            );
+        }
     }
 
     Events.emit('Players_isOnline_changed', name, true);
@@ -30,7 +46,7 @@ Events.on('minecraft_logs_playerDisconnected', (name, xuid) => {
     Events.emit('Players_isOnline_changed', name, false);
 });
 
-async function writeWhitelist(){
+async function writeWhitelist() {
     const whitelistFile = [];
     const players = await Database.players.getAll();
     players.forEach(el => {
@@ -45,29 +61,10 @@ async function writeWhitelist(){
     await writeFile('mc/bedrock-server/whitelist.json', JSON.stringify(whitelistFile));
     Minecraft.process.stdin.write('whitelist reload\n');
 }
-
-async function pardon(name){
-    const player = await Database.players.get(name);
-    player.ban = false;
-    await Database.players.update(name, player);
-    await writeWhitelist();
-}
-async function ban(name, ban){
-    const player = await Database.players.get(name);
-    player.ban = ban;
-    Database.players.update(name, player);
-    Minecraft.process.stdin.write(`kick ${player.name} You have been banned: ${player.ban.reason}\n`);
-    await writeWhitelist();
-}
-async function changePermission(name, permission){
-    const player = await Database.players.get(xuid);
-    player.permission = permission;
-    Database.players.update(name, player);
-
+async function writePermissionsFile() {
     const players = await Database.players.getAll();
-    const permissionsFile = players.map(el => {
+    const permissionsFile = players.filter(el => el.xuid).map(el => {
         return {
-            name: el.name,
             xuid: el.xuid,
             permission: el.permission
         }
@@ -75,7 +72,29 @@ async function changePermission(name, permission){
     await writeFile('mc/bedrock-server/permissions.json', JSON.stringify(permissionsFile));
     Minecraft.process.stdin.write('permission reload\n');
 }
-async function add(name, permission){
+
+async function pardon(name) {
+    const player = await Database.players.get(name);
+    player.ban = false;
+    await Database.players.update(name, player);
+    await writeWhitelist();
+}
+async function ban(name, ban) {
+    const player = await Database.players.get(name);
+    player.ban = ban;
+    Database.players.update(name, player);
+    Minecraft.process.stdin.write(`kick ${player.name} You have been banned: ${player.ban.reason}\n`);
+    await writeWhitelist();
+}
+async function changePermission(name, permission) {
+    const player = await Database.players.get(name);
+    player.permission = permission;
+    Database.players.update(name, player);
+
+    const players = await Database.players.getAll();
+    await writePermissionsFile();
+}
+async function add(name, permission) {
     const player = await Database.players.insert({name, permission});
     Events.emit('players_newPlayer', player);
     await writeWhitelist();
