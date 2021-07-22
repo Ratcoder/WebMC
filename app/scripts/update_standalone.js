0a49273ad753db11a50a5dd77034d5be17f0b6a9
Ratcoder <69884239+Ratcoder@users.noreply.github.com>
2021-07-21 22:42:46 -0500

    added automatic updates

---
+++ app/scripts/update_standalone.js
@@ -0,0 +1,37 @@
+const fs = require('fs').promises;
+const child_process = require('child_process');
+const fetch = require('node-fetch');
+
+(async () => {
+    const currentManifest = JSON.parse(await fs.readFile('manifest.json'));
+    const newManifest = await (await fetch('https://webmc.ratcoder.com/bin/latest/manifest.json')).json();
+    async function diff(newFolder, oldFolder, rootPath = '', downloadFolder = false) {
+        for (const key in newFolder) {
+            if (Object.hasOwnProperty.call(newFolder, key)) {
+                const file = newFolder[key];
+                if (typeof file == "object") {
+                    if (downloadFolder) {
+                        await diff(file, null, rootPath + '/' + key, true);
+                    }
+                    else if (oldFolder[key] && typeof oldFolder[key] == "object") {
+                        await diff(file, oldFolder[key], rootPath + '/' + key);
+                    }
+                    else {
+                        await diff(file, null, rootPath + '/' + key, true);
+                    }
+                }
+                else if (downloadFolder || !oldFolder[key] || oldFolder[key] != newFolder[key]) {
+                    // download new file
+                    console.log(`${rootPath}/${key}`);
+                    await fs.writeFile(`${process.cwd()}${rootPath}/${key}`, await (await fetch(`https://webmc.ratcoder.com/bin/latest${rootPath}/${key}`)).text());
+                    console.log(`${rootPath}/${key}`);
+                }
+            }
+        }
+    }
+    await diff(newManifest.files, currentManifest.files);
+    await fs.writeFile(JSON.stringify(newManifest), 'manifest.json');
+    await fs.rmdir('temp', { recursive: true });
+    child_process.spawn(process.cwd() + '/start_webmc.' + ((process.platform == 'win32') ? 'bat' : 'sh'), [], { detached: true });
+    process.exit();
+})()
---
+++ app/services/updates.js
@@ -0,0 +1,39 @@
+const child_process = require('child_process');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const node_schedule = require('node-schedule');
+const fetch = require('node-fetch');
+const Minecraft = require('./minecraft');
+const currentVersion = '0.0.0';
+
+if (process.argv.find(el => el == '--dev')) {
+    return;
+}
+
+checkForUpdates();
+node_schedule.scheduleJob('0 0 * * *', checkForUpdates); // Every day at midnight
+
+function checkForUpdates() {
+    if (getLatestVersion() != currentVersion) {
+        Minecraft.sceduleOffJob(async () => {
+            fs.mkdirSync('temp');
+            fs.copyFileSync('app/scripts/update_standalone.js', 'temp/update_standalone.js');
+            if (os.platform() == 'win32') {
+                fs.writeFileSync('temp/update.bat', '@node\\node.exe temp\\update_standalone.js');
+                child_process.spawn(__dirname + '/../../temp/update.bat', ['temp/update_standalone.js'], { detached: true });
+            }
+            else {
+                fs.writeFileSync('temp/update.sh', '@node\\node temp\\update_standalone.js');
+                child_process.spawn(__dirname + '/../../temp/update.sh', [], { detached: true });
+            }
+            process.exit();
+        }, "updating WebMC");
+    }
+}
+
+async function getLatestVersion(){
+    const res = await fetch('https://webmc.ratcoder.com/bin/version.txt');
+    const text = await res.text();
+    return text;
+}
